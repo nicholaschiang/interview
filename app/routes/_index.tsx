c3cf5a2a51bfd5de8b5ee17e752d760ea122f852
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-08-01 02:32:18 -0600

    feat(routes/index): update home page description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from '@vercel/remix'
+import { Link } from '@remix-run/react'
+import { type MetaFunction } from '@vercel/remix'
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,13 +11,37 @@ export const meta: MetaFunction = () => {
 export default function IndexPage() {
   return (
     <div className='flex flex-col items-center justify-center gap-2 px-4 py-12'>
-      <h1 className='text-blue-500'>Welcome to Remix</h1>
+      <h1 className='text-xl font-medium text-green-700'>Interview</h1>
+      <p>
+        This website contains some templates and API endpoints for use during
+        frontend coding interviews.
+      </p>
+      <p>
+        <Link to='/products' className='text-blue-500'>
+          Here
+        </Link>{' '}
+        is an example solution UI. For more details, please visit{' '}
+        <a
+          href='https://github.com/nicholaschiang/interview#readme'
+          target='_blank'
+          rel='noreferrer'
+          className='text-blue-500'
+        >
+          the GitHub repository
+        </a>
+        .
+      </p>
+      <div className='h-48' />
+      <h2 className='font-medium text-green-700'>Welcome to Remix</h2>
+      <p>This website uses Remix as the framework of choice.</p>
+      <p>To learn more, please reference the following links:</p>
       <ul className='list-disc'>
         <li>
           <a
             target='_blank'
             href='https://remix.run/tutorials/blog'
             rel='noreferrer'
+            className='text-blue-500'
           >
             15m Quickstart Blog Tutorial
           </a>
@@ -26,12 +51,18 @@ export default function IndexPage() {
             target='_blank'
             href='https://remix.run/tutorials/jokes'
             rel='noreferrer'
+            className='text-blue-500'
           >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+          <a
+            target='_blank'
+            href='https://remix.run/docs'
+            rel='noreferrer'
+            className='text-blue-500'
+          >
             Remix Docs
           </a>
         </li>
